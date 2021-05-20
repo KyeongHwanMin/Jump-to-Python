d7f0fcdb442a6da49dee2629edf3bb4330808595
KyeongHwanMin <h45652@naver.com>
2021-05-20 18:33:22 +0900

    파이썬 첫번째 예제

---
+++ ex1.py
@@ -0,0 +1,20 @@
+print("hello")
+
+print("Mary`s cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야".')
+
+print('"C:\Wiondows"')
+
+print("안녕하세요.\n만나서 \t \t반갑습니다.")
+
+print("오늘은","일요일")
+
+print("naver","kakao","sk","samsung",sep=";")
+#print 함수의 sep 인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론이 출력됩니다.
+
+print("naver","kakao","sk","samsung",sep="/")
+
+print("first",end="");print("second")
+
+print(5/3)
