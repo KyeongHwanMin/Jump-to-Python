026b3d7968c83fda99ac7d15e66785d0398e0dc4
KyeongHwanMin <h45652@naver.com>
2021-09-30 21:37:30 +0900

    refactor(ex9.py) : 함수 연습문제221~230

--- ex9.py
+++ ex9.py
@@ -121,3 +121,65 @@ print_max(10,20,30)
 def print_max1(a,b,c):
     print(max(a,b,c))
 print_max1(10,20,30)
+
+#221 입력된 문자열을 역순으로 출력하는 print_reverse 함수를 정의하라.
+def print_reverse(string):
+    print(string[::-1])
+print_reverse("문자")
+#222 성적 리스트를 입력 받아 평균을 출력하는 print_score 함수를 정의하라.
+def print_score(list):
+    print(sum(list)/len(list))
+print_score([1,2,3])
+#223 하나의 리스트를 입력받아 짝수만 화면에 출력하는 print_even 함수를 정의하라.
+def print_even(list):
+    for i in list:
+        if i % 2 == 0:
+            print(i)
+print_even([1,3,2,10,12,11,15])
+#224 하나의 딕셔너리를 입력받아 딕셔너리의 key 값을 화면에 출력하는 print_keys 함수를 정의하라.
+def print_keys(dict):
+    for i in dict.keys():
+        print(i)
+print_keys({"이름":"길말똥","나이":30,"성별":0})
+#225 my_dict와 날짜 키값을 입력받아 OHLC 리스트를 출력하는 print_value_by_key 함수를 정의하라.
+my_dict = {"10/26" : [100, 130, 100, 100],
+           "10/27" : [10, 12, 10, 11]}
+def print_value_by_key(a,b):
+    print(my_dict[b])
+print_value_by_key(my_dict,"10/26")
+#226 입력 문자열을 한 줄에 다섯글자씩 출력하는 print_5xn(string) 함수를 작성하라.
+def print_5xn(a):
+    times = len(a)/5
+    times=int(times+0.9) # 11/5=2.1+0.9=3.0
+    for i in range(times):     #i->0, 1
+       print(a[5*i:5*i+5])    #[0:5],[5:10]
+
+print_5xn("아이엠어보이유알어걸추가")
+#227 문자열과 한줄에 출력될 글자 수를 입력을 받아 한 줄에 입력된 글자 수만큼 출력하는 print_mxn(string) 함수를 작성하라.
+import math
+def printmxn(data,num):
+    #횟수를 계산
+    tiems = len(data)/num
+    times = math.ceil(tiems) # 올림 함수
+
+    for i in range(times):
+        print(data[i*num : (i+1)*num]) # num=3, i=0, [0:3]
+printmxn("아이엠어보이유알어걸", 3)
+#228 연봉을 입력받아 월급을 계산하는 calc_monthly_salary(annual_salary) 함수를 정의하라. 회사는 연봉을 12개월로 나누어 분할 지급하며, 이 때 1원 미만은 버림한다.
+def calc_monthly_salary(annual_salary):
+    money = int(annual_salary / 12)
+    print(money)
+
+calc_monthly_salary(12000000)
+#229
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(a=100, b=200)
+#230
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(b=100, a=200)
