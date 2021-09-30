c393242af81e9896c28f63751003cfe95911d74a
KyeongHwanMin <h45652@naver.com>
2021-09-30 18:11:33 +0900

    파이썬 반복문 문제 풀이 171~180

--- ex8.py
+++ ex8.py
@@ -166,5 +166,68 @@ b=1
 for a in range(1,11):
     b*=a
 print("곱",b)
+#171 리스트의 데이터를 출력하라. 단, for문과 range문을 사용하라.
+price_list = [32100, 32150, 32000, 32500]
+for i in range(len(price_list)):
+    print(price_list[i])
+#172 리스트의 데이터를 출력하라. 단, for문과 range문을 사용하라.
+price_list = [32100, 32150, 32000, 32500]
+for i in range(len(price_list)):
+    print(i,price_list[i])
+#173
+price_list = [32100, 32150, 32000, 32500]
+for i in range(len(price_list)):
+    print((len(price_list)-1)-i,price_list[i])
+#174
+price_list = [32100, 32150, 32000, 32500]
+for i in range(1,4):
+    print(90+10*i,price_list[i])
+#175 my_list를 아래와 같이 출력하라.
+my_list = ["가", "나", "다", "라"]
+for i in [0,1,2] :
+    print(my_list[i]+' '+ my_list[i+1])
+my_list1 = ["가", "나", "다", "라"]
+for i in range(len(my_list1)-1) :
+    print(i,my_list1[i], my_list1[i+1])
+my_list2 = ["가", "나", "다", "라"]
+for i in range(1, len(my_list2) ) :
+  print(my_list2[i-1], my_list2[i])
+my_list3 = ["가", "나", "다", "라"]
+for i in range(len(my_list3) - 1 ) :
+  print(my_list3[i], my_list3[i+1])
+#176
+my_list = ["가", "나", "다", "라", "마"]
+for i in range(len(my_list)-2):
+    print(my_list[i],my_list[i+1],my_list[i+2])
+for i in range(1, len(my_list) - 1):
+    print(my_list[i - 1], my_list[i], my_list[i + 1])
+for i in range(2, len(my_list)):
+    print(my_list[i - 2], my_list[i - 1], my_list[i])
+#177
+my_list = ["가", "나", "다", "라"]
+for i in range(len(my_list) - 1, 0, -1):
+    print(my_list[i], my_list[i-1])
+for i in range(len(my_list) - 1):
+    print(my_list[len(my_list) - 1 - i], my_list[len(my_list) - 2 - i])
+#178
+my_list = [100, 200, 400, 800]
+for i in range(len(my_list)-1):
+    print(my_list[i+1]-my_list[i])
+#179
+my_list = [100, 200, 400, 800, 1000, 1300]
+for i in range(1,len(my_list)-1) :
+    print((my_list[i-1]+my_list[i]+my_list[i+1]) / 3)
+#180
+low_prices  = [100, 200, 400, 800, 1000]
+high_prices = [150, 300, 430, 880, 1000]
+volatility =[]
+for i in range(len(high_prices)) :
+    volatility.append(high_prices[i] - low_prices[i])
+print(volatility)
+
+
+
+
+
 
 
