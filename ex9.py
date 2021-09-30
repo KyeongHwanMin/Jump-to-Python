aa71b8c1f90c1ce2cfe00eb2ef90efa2335c410a
KyeongHwanMin <h45652@naver.com>
2021-09-30 22:32:20 +0900

    refactor(ex9.py) : 함수 연습문제231~240

--- ex9.py
+++ ex9.py
@@ -183,3 +183,80 @@ def my_print (a, b) :
     print("오른쪽:", b)
 
 my_print(b=100, a=200)
+#231
+# def n_plus_1 (n) :
+#     result = n + 1
+#
+# n_plus_1(3)
+# print (result)
+# 함수 내부에서 사용한 변수는 함수 밖에서는 접근이 불가능합니다. (문법이 그래요) 함수 내부에서 계산한 값을 전달하기 위해서는 return을 사용해야 합니다.
+#232 문자열 하나를 입력받아 인터넷 주소를 반환하는 make_url 함수를 정의하라.
+def make_url(a):
+    return(f"www.{a}.com")
+make_url("naver")
+#233 문자열을 입력받아 각 문자들로 구성된 리스트로 반환하는 make_list 함수를 정의하라.
+def make_list(a):
+    print(list(a))
+make_list("abcd")
+#234 숫자로 구성된 하나의 리스트를 입력받아, 짝수들을 추출하여 리스트로 반환하는 pickup_even 함수를 구현하라.
+def pickup_even(list):
+    result = []
+    for i in list:
+        if i % 2==0:
+            result.append(i)
+pickup_even([3, 4, 5, 6, 7, 8])
+#235 콤마가 포함된 문자열 숫자를 입력받아 정수로 변환하는 convert_int 함수를 정의하라.
+def convert_int(num):
+    num=num.replace(",",'')
+    print(int(num))
+convert_int("1,234,567")
+#236
+def 함수(num) :
+    return num + 4
+
+a = 함수(10)
+b = 함수(a)
+c = 함수(b)
+print(c)
+#237
+def 함수(num) :
+    return num + 4
+
+c = 함수(함수(함수(10)))
+print(c)
+#238
+def 함수1(num) :
+    return num + 4
+
+def 함수2(num) :
+    return num * 10
+
+a = 함수1(10)
+c = 함수2(a)
+print(c)
+#239
+def 함수1(num) :
+    return num + 4
+
+def 함수2(num) :
+    num = num + 2
+    return 함수1(num)
+
+c = 함수2(10)
+print(c)
+# 240
+def 함수0(num) :
+    return num * 2
+
+def 함수1(num) :
+    return 함수0(num + 2)
+
+def 함수2(num) :
+    num = num + 10
+    return 함수1(num)
+
+c = 함수2(2)
+print(c)
+
+
+
