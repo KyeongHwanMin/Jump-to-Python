4d4aabd4cd52abccf0d97761c7f6c53b703f5464
KyeongHwanMin <h45652@naver.com>
2021-09-30 18:59:52 +0900

    파이썬 반복문 문제 풀이 191~200

--- ex8.py
+++ ex8.py
@@ -266,6 +266,91 @@ for i in apart:
     for j in i:
         print(j,'호')
 print('-----')
+#191
+data = [
+    [ 2000,  3050,  2050,  1980],
+    [ 7500,  2050,  2050,  1980],
+    [15450, 15050, 15550, 14900]
+]
+for i in data:
+    for j in i:
+        print(j+(j*0.014*0.01))
+#192  191번의 출력 결과에 행단위로 "----" 구분자를 추가하라.
+for i in data:
+    for j in i:
+        print(j+(j*0.014*0.01))
+    print("-"*5)
+#193 192 번 문제의 결괏값을 result 이름의 리스트에 1차원 배열로 저장하라.
+result=[]
+for i in data:
+    for j in i:
+        result.append(j + (j * 0.014 * 0.01))
+print(result)
+#194 191번 문제의 결괏값을 result 이름의 리스트에 2차원 배열로 저장하라. 저장 포맷은 아래와 같다. 각 행에 대한 데이터끼리 리스트에 저장되어야 한다.
+data = [
+    [ 2000,  3050,  2050,  1980],
+    [ 7500,  2050,  2050,  1980],
+    [15450, 15050, 15550, 14900]
+]
+result = []
+for price in data:
+    day_price = []
+    for price in price:
+        new_p = price + (price * 0.014 *0.01)
+        day_price.append(new_p)
+    result.append(day_price)
+print(result)
+#195 ohlc 리스트에는 시가(open), 고가 (high), 저가 (low) , 종가(close)가 날짜별로 저장돼 있다. 화면에 종가데이터를 출력하라.
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    print(i[3])
+#196 종가가 150원보다 큰경우에만 종가를 출력하라.
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    if i[3] > 150:
+        print(i[3])
+#197 종가가 시가 보다 크거나 같은 경우에만 종가를 출력하라.
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    if i[3] >= i[0]:
+        print(i[3])
+#198  고가와 저가의 차이를 변동폭으로 정의할 때 변동폭을 volatility 이름의 리스트에 저장하라.
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+volatility=[]
+for i in ohlc[1:]:
+    volatility.append(i[1]-i[2])
+print(volatility)
+#199 종가가 시가보다 높은 날의 변동성 (고가 - 저가)을 화면에 출력하라.
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for row in ohlc[1:]:
+    if row[3] > row[0]:
+        print(row[1]-row[2])
+#200 시가에 매수해서 종가에 매도 했을 경우 총 수익금을 계산하라.
+hap =0
+for i in ohlc[1:]:
+     cu = i[0]-i[3]
+     hap += cu
+print(hap)
+
+
+
+
+
 
 
 
