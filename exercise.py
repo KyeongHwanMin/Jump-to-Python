c81df0d714d9ffe6a63eed718096245616e76b26
KyeongHwanMin <h45652@naver.com>
2021-11-15 18:35:26 +0900

    refactor(exercise.py) : 연습 문제: 얌체공

--- exercise.py
+++ exercise.py
@@ -38,9 +38,25 @@
 # 정수를 한 개 입력받아, 1부터 입력받은 수까지 각각에 대해 제곱을 구해 프린트하는 프로그램을 작성해 보세요.
 # 단, while 문을 사용하세요.
 
-print("정수를 입력하시오. :")
-num = int(input())
-i = 1
-while i < num:
-    print(i,i*i)
-    i+=1
+# print("정수를 입력하시오. :")
+# num = int(input())
+# i = 1
+# while i < num:
+#     print(i,i*i)
+#     i+=1
+#
+# 2.1.3 연습 문제: 얌체공
+# 문제1
+# 고무 공을 100 미터 높이에서 떨어뜨리는데, 이 공은 땅에 닿을 때마다 원래 높이의 3/5 만큼 튀어오릅니다.
+# 공이 열 번 튈 동안, 그때마다 공의 높이를 계산합니다.
+# height = 100
+# bounce = 3 / 5
+#
+# i = 1
+#
+# while i <= 10:
+#     height = height * bounce
+#     print(i, round(height,4))
+#     i = i +1
+
+
